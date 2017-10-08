04a569c6cae0ce597061cd4aad2a669d0eb8e030
Arian <arianmarkus77@yahoo.com>
2017-10-08 19:23:24 +0700

    Calculator time

---
+++ index.js
@@ -0,0 +1,15 @@
+var someMillisecondValue = 511111222127;
+var date = new Date();
+var minute = date.getMinutes();
+var hour = date.getHours();
+var day = date.getDate();
+var month = date.getMonth();
+var year = date.getFullYear();
+
+console.log(someMillisecondValue);
+console.log("Date:", date);
+console.log("Minute:", minute);
+console.log("Hour:", hour);
+console.log("Day:", day);
+console.log("Month:", month);
+console.log("Year:", year);
