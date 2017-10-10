e4fe168cfcf388f18a33ecb30bd84aac296a65dc
Andrew <32459976+andreewww56@users.noreply.github.com>
2017-10-10 15:41:58 +0700

    calculator time

---
+++ index.js
@@ -0,0 +1,18 @@
+var date = new Date();
+var minute = date.getMinutes();
+var hour = date.getHours();
+var day = date.getDate();
+
+console.log(date);
+console.log(hour);
+console.log(minute);
+
+document.getElementById('andrew').textContent = date
+document.getElementById('andrew').value
+
+// var date = new Date("Monday, october 9, 2017");
+// day = date.getDay();
+
+// console.log(day);
+
+document.querySelectorAll('#id')
